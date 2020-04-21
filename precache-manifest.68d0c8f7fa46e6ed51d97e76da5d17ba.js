100cbe560043f372dd5213119d64b1fb2b998848
dvru <damvaru4@gmail.com>
2020-04-20 19:23:59 -0500

    Updates

---
+++ precache-manifest.68d0c8f7fa46e6ed51d97e76da5d17ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95924abf949706d9946082f9f89ef750",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "5e78bd10e7a02cc93ccc",
+    "url": "/facerecognitionbrain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "474df7f807eaa0b694d1",
+    "url": "/facerecognitionbrain/static/css/main.6114f8a7.chunk.css"
+  },
+  {
+    "revision": "5e78bd10e7a02cc93ccc",
+    "url": "/facerecognitionbrain/static/js/2.017b0ab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionbrain/static/js/2.017b0ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474df7f807eaa0b694d1",
+    "url": "/facerecognitionbrain/static/js/main.b452710b.chunk.js"
+  },
+  {
+    "revision": "cf6990c032c001c83906",
+    "url": "/facerecognitionbrain/static/js/runtime-main.fdc8abc9.js"
+  }
+]);
